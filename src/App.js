b7f31667872ec2929958dd975aa394c32177148f
Chris Whong <chris@whong.org>
2019-07-31 23:24:15 -0400

    set up chart with static data

--- src/App.js
+++ src/App.js
@@ -1,26 +1,110 @@
 import React from 'react';
-import logo from './logo.svg';
+import convert from 'xml-js';
+import * as d3 from 'd3';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  async componentDidMount() {
+    const xml = await fetch('/data/ai-signature-saxby.aip').then(d => d.text())
+    const json = convert.xml2js(xml, { compact: true })
+
+    // chart based on https://bl.ocks.org/gordlea/27370d1eea8464b04538e6d8ced39e89
+
+    // TODO get the width of the container and do everything dynamically
+    const margin = {top: 50, right: 50, bottom: 50, left: 50}
+    const width = 960 - margin.left - margin.right
+    const height = 500 - margin.top - margin.bottom
+
+    const xMax = 1440 // x axis is # of minutes in a day
+    const yMax = 2000 // assume 2000 is max y, not sure what max intensity can be
+    const xTickValues = [0, 240, 480, 720, 960, 1200, 1440]
+    const yTickValues = [0, 500, 1000, 1500, 2000]
+
+    const xScale = d3.scaleLinear()
+      .domain([0, xMax - 1])
+      .range([0, width]);
+
+
+    const yScale = d3.scaleLinear()
+      .domain([0, yMax])
+      .range([height, 0]);
+
+    const line = d3.line()
+      .x(function(d) { return xScale(parseInt(d.time._text)); })
+      .y(function(d) { return yScale(parseInt(d.intensity._text)); })
+      .curve(d3.curveMonotoneX)
+
+    const svg = d3.select('.chart').append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    // x axis
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(
+          d3.axisBottom(xScale)
+            .tickValues(xTickValues)
+            .tickSize(0)
+            .tickFormat((d) => {
+              // handle midnight
+              if (d === 0 || d === 1440) return '12AM'
+              // handle noon
+              if (d === 720) return '12AM'
+              // handle pm
+              if (d > 720) return `${(d - 720) / 60}PM`
+              return `${d / 60}AM`
+            }));
+    //
+    // // y axis
+    // svg.append("g")
+    //     .attr("class", "y axis")
+    //     .call(d3.axisLeft(yScale));
+
+    // add vertical gridlines
+    svg.append("g")
+      .attr("class", "grid")
+      .attr("transform", "translate(0," + height + ")")
+      .call(
+        d3.axisBottom(xScale)
+          .tickValues(xTickValues)
+          .tickSize(-height)
+          .tickFormat("")
+      )
+      .call(g => g.select('.domain').remove())
+
+    // add horizontal gridlines
+    svg.append("g")
+      .attr("class", "grid")
+      .call(
+        d3.axisLeft(yScale)
+          .tickValues(yTickValues)
+          .tickSize(-width)
+          .tickFormat("")
+      )
+      .call(g => g.select('.domain').remove())
+
+    // for each color in the data, render a line
+    const colors = Object.keys(json.ramp.colors);
+    colors.forEach((color) => {
+      const colorData = json.ramp.colors[color].point
+      svg.append("path")
+        .datum(colorData)
+        .attr("class", `line ${color}`)
+        .attr("d", line);
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="chart">
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
